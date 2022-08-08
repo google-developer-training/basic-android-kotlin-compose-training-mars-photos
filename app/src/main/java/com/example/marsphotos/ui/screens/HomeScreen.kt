163302147e3c73aebcf0c193e975f1748e76d45e
Anuj Garg <garganuj@google.com>
2022-08-08 09:54:31 +0530

    Starter code for coil codelab ABC 5.2.2
    Change-Id: If53a8fa7cd55359fd15781c25eaffa621c1a6a44

--- app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -19,31 +19,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Button
-import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.example.marsphotos.R
-import com.example.marsphotos.model.MarsPhoto
 import com.example.marsphotos.ui.theme.MarsPhotosTheme
 
 @Composable
@@ -54,7 +41,7 @@ fun HomeScreen(
 ) {
     when (marsUiState) {
         is MarsUiState.Loading -> LoadingScreen(modifier)
-        is MarsUiState.Success -> PhotosGridScreen(marsUiState.photos, modifier)
+        is MarsUiState.Success -> ResultScreen(marsUiState.photos, modifier)
         else -> ErrorScreen(retryAction, modifier)
     }
 }
@@ -94,40 +81,15 @@ fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
 }
 
 /**
- * The home screen displaying photo grid.
+ * ResultScreen displaying number of photos retrieved.
  */
 @Composable
-fun PhotosGridScreen(photos: List<MarsPhoto>, modifier: Modifier = Modifier) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(150.dp),
-        modifier = modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(4.dp)
+fun ResultScreen(photos: String, modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
     ) {
-        items(photos, key = { photo -> photo.id }) { photo ->
-            MarsPhotoCard(photo)
-        }
-    }
-}
-
-@Composable
-fun MarsPhotoCard(photo: MarsPhoto, modifier: Modifier = Modifier) {
-    Card(
-        modifier = modifier
-            .padding(4.dp)
-            .fillMaxWidth()
-            .aspectRatio(1f),
-        elevation = 8.dp,
-    ) {
-        AsyncImage(
-            model = ImageRequest.Builder(context = LocalContext.current)
-                .data(photo.imgSrc)
-                .crossfade(true)
-                .build(),
-            fallback = painterResource(R.drawable.ic_broken_image),
-            placeholder = painterResource(R.drawable.loading_img),
-            contentDescription = stringResource(R.string.mars_photo),
-            contentScale = ContentScale.FillBounds,
-        )
+        Text(photos)
     }
 }
 
@@ -149,9 +111,8 @@ fun ErrorScreenPreview() {
 
 @Preview(showBackground = true)
 @Composable
-fun PhotosGridScreenPreview() {
+fun ResultScreenPreview() {
     MarsPhotosTheme {
-        val mockData = List(10) { MarsPhoto("$it", "") }
-        PhotosGridScreen(mockData)
+        ResultScreen(stringResource(R.string.placeholder_success))
     }
 }
