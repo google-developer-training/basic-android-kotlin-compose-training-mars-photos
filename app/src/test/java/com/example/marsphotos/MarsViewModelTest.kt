fbe6741bf3d0b458f0d72fe17ff3a6e1839d1029
schordas <schordas@google.com>
2022-08-31 12:43:45 -0700

    Added local tests
    Change-Id: Iace4df68f777c41fe49f7afce13b3123ce11532f

---
+++ app/src/test/java/com/example/marsphotos/MarsViewModelTest.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.marsphotos
+
+import com.example.marsphotos.fake.FakeDataSource
+import com.example.marsphotos.fake.FakeNetworkMarsPhotosRepository
+import com.example.marsphotos.rules.TestDispatcherRule
+import com.example.marsphotos.ui.screens.MarsUiState
+import com.example.marsphotos.ui.screens.MarsViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MarsViewModelTest {
+
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
+    @Test
+    fun marsViewModel_getMarsPhotos_verifyMarsUiStateSuccess() =
+        runTest {
+            val marsViewModel = MarsViewModel(
+                marsPhotosRepository = FakeNetworkMarsPhotosRepository()
+            )
+            assertEquals(
+                MarsUiState.Success("Success: ${FakeDataSource.photosList.size} Mars " +
+                        "photos retrieved"),
+                marsViewModel.marsUiState
+            )
+        }
+}
---
+++ app/src/test/java/com/example/marsphotos/NetworkMarsRepositoryTest.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.marsphotos
+
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import com.example.marsphotos.fake.FakeDataSource
+import com.example.marsphotos.fake.FakeMarsApiService
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class NetworkMarsRepositoryTest {
+
+    @Test
+    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
+        runTest {
+            val repository = NetworkMarsPhotosRepository(
+                marsApiService = FakeMarsApiService()
+            )
+            assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
+        }
+}
