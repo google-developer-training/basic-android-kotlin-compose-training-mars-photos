a8363ae79308608b4cec9af21b41830e9cb45447
Android Dev <jtavva@google.com>
2022-10-10 15:37:21 -0700

    Revert "Revert "Use kotlinx.serialization for json parsing" (#3)"
    This reverts commit cac718b346c939245c6373b4b4e198b2d0cd044c.

--- app/src/main/java/com/example/marsphotos/model/MarsPhoto.kt
+++ app/src/main/java/com/example/marsphotos/model/MarsPhoto.kt
@@ -16,13 +16,15 @@
 
 package com.example.marsphotos.model
 
-import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 /**
  * This data class defines a Mars photo which includes an ID, and the image URL.
  */
+@Serializable
 data class MarsPhoto(
     val id: String,
-    @SerializedName(value = "img_src")
+    @SerialName("img_src")
     val imgSrc: String
 )
