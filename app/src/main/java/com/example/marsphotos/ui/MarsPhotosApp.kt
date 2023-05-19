97230328c6c8b3baa9b8a392937322784171e713
Android Dev <androidedu@google.com>
2023-05-18 17:01:43 -0700

    Update MarsPhotosApp.kt

--- app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
+++ app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
@@ -48,10 +48,7 @@ fun MarsPhotosApp() {
         ) {
             val marsViewModel: MarsViewModel =
                 viewModel(factory = MarsViewModel.Factory)
-            HomeScreen(
-                marsUiState = marsViewModel.marsUiState,
-                retryAction = marsViewModel::getMarsPhotos
-            )
+            HomeScreen(marsUiState = marsViewModel.marsUiState)
         }
     }
 }
