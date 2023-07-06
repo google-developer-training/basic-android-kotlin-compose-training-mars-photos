ff3001a0cfb91aa20972c2d0801b4ed0d42e447f
Android dev <androidedu@google.com>
2023-07-06 09:17:22 -0500

    Feedback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.7"
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -72,7 +72,6 @@ dependencies {
 
     // Import the Compose BOM
     implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
