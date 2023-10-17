5e87e94d32b8616696bb8e67f4b9b8f2a4fc22aa
Android Dev <testingilt567@gmail.com>
2023-10-16 17:11:04 -0700

    Use new API

--- app/src/main/java/com/example/marsphotos/MainActivity.kt
+++ app/src/main/java/com/example/marsphotos/MainActivity.kt
@@ -19,16 +19,16 @@ package com.example.marsphotos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.core.view.WindowCompat
 import com.example.marsphotos.ui.MarsPhotosApp
 import com.example.marsphotos.ui.theme.MarsPhotosTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             MarsPhotosTheme {
