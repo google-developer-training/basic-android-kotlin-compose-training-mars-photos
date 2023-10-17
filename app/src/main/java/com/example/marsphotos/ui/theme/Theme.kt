7ed7b9f311991626929db151e57e444414a3529f
Android Dev <testingilt567@gmail.com>
2023-10-16 17:11:04 -0700

    Use new API

--- app/src/main/java/com/example/marsphotos/ui/theme/Theme.kt
+++ app/src/main/java/com/example/marsphotos/ui/theme/Theme.kt
@@ -16,9 +16,7 @@
 
 package com.example.marsphotos.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import android.view.View
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -26,12 +24,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -62,12 +55,6 @@ fun MarsPhotosTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            setUpEdgeToEdge(view, darkTheme)
-        }
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
@@ -75,24 +62,4 @@ fun MarsPhotosTheme(
         shapes = Shapes,
         content = content
     )
-}
-
-/**
- * Sets up edge-to-edge for the window of this [view]. The system icon colors are set to either
- * light or dark depending on whether the [darkTheme] is enabled or not.
- */
-private fun setUpEdgeToEdge(view: View, darkTheme: Boolean) {
-    val window = (view.context as Activity).window
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-    window.statusBarColor = Color.Transparent.toArgb()
-    val navigationBarColor = when {
-        Build.VERSION.SDK_INT >= 29 -> Color.Transparent.toArgb()
-        Build.VERSION.SDK_INT >= 26 -> Color(0xFF, 0xFF, 0xFF, 0x63).toArgb()
-        // Min sdk version for this app is 24, this block is for SDK versions 24 and 25
-        else -> Color(0x00, 0x00, 0x00, 0x50).toArgb()
-    }
-    window.navigationBarColor = navigationBarColor
-    val controller = WindowCompat.getInsetsController(window, view)
-    controller.isAppearanceLightStatusBars = !darkTheme
-    controller.isAppearanceLightNavigationBars = !darkTheme
 }
