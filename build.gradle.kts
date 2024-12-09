4e02933d446efd57824138a4a623731248facaa0
Jose Alcérreca <jalc@google.com>
2024-12-09 13:11:08 +0000

    Compose compiler plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("com.android.library") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
